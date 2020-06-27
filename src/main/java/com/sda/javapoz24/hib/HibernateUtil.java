b38a0bb4e3e7c71b128e1c537b92cf5095a8e399
Paweł Recław <pawel.reclaw@gmail.com>
2020-06-27 11:26:45 +0200

    Hibernate connecting

---
+++ src/main/java/com/sda/javapoz24/hib/HibernateUtil.java
@@ -0,0 +1,30 @@
+package com.sda.javapoz24.hib;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+// SINGLETON
+public class HibernateUtil {
+    private static final SessionFactory SESSION_FACTORY;
+
+    //    Sekcja static - wywołuje się po starcie aplikacji. wykonuje się jeden raz
+    static {
+        // pobieramy konfigurację i ustawiamy SESSION_FACTORY;
+        try {
+            // ta konfiguracja ładuje plik hibernate.cfg.xml
+            Configuration configuration = new Configuration();
+            configuration.configure("/hibernate.cfg.xml"); // nie muszę tego wpisywać, domyślnie ładuje ten plik
+
+            SESSION_FACTORY = configuration.buildSessionFactory();
+        } catch (HibernateException hibernateException) {
+            System.err.println("Błąd inicjalizacji sesji.");
+            hibernateException.printStackTrace(); // wypisz na ekran
+            throw hibernateException; // rzuć wyjątek wyżej
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+}
--- src/main/java/com/sda/javapoz24/hib/Main.java
+++ src/main/java/com/sda/javapoz24/hib/Main.java
@@ -1,7 +1,10 @@
 package com.sda.javapoz24.hib;
 
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
+        SessionFactory factory = HibernateUtil.getSessionFactory();
 
     }
 }
